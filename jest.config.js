b5255bd54d8f62176f9c8568f837c57348089b8a
宗金运 <zongjinyun@welljoint.com>
2020-12-29 16:10:38 +0800

    test:增加了fs-callid和counter的部分测试;增加test.js设置jest环境下的loglevel变量,防止报错

--- jest.config.js
+++ jest.config.js
@@ -116,7 +116,7 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    "src"
+    "src","routes"
   ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
